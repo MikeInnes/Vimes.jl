ae36321aad9fa401b28251b5dcd90ed37a32a78a
Mike J Innes <mike.j.innes@gmail.com>
2019-05-22 15:48:24 +0100

    better CSTParser error message

--- src/Vimes.jl
+++ src/Vimes.jl
@@ -1,10 +1,15 @@
 module Vimes
 
-using MacroTools, CSTParser, StatsBase, Dates, Printf
-using MacroTools: sourcemap
+using CSTParser
 
 isdefined(CSTParser, :LocExpr) ||
-  error("] add https://github.com/MikeInnes/CSTParser.jl#location")
+  error("""
+  Vimes depends on a fork of CSTParser:
+  ] add https://github.com/MikeInnes/CSTParser.jl#location
+  """)
+
+using MacroTools, CSTParser, StatsBase, Dates, Printf
+using MacroTools: sourcemap
 
 include("library.jl")
 include("patch.jl")
